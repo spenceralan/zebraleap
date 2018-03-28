e1ba3435901e8000d125b4d0efaf29795ef1ea72
Anthony Panozzo <panozzaj@gmail.com>
2018-03-28 14:08:36 -0400

    Remove tests

--- test/controllers/products_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get buy" do
-    get products_buy_url
-    assert_response :success
-  end
-
-end
