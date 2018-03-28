d49813980e70a30e91901e5e2ee314177d5e3228
Anthony Panozzo <panozzaj@gmail.com>
2018-03-28 13:18:42 -0400

    Change seed to be more Lessonly specific

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Product.find_or_create_by name: 'Fancy bottle of sparkling water' do |product|
-  product.price_in_cents = 5000
+Product.find_or_create_by name: 'Premium - Ruby on Rails lesson' do |product|
+  product.price_in_cents = 50000
 end
 
 User.find_or_create_by name: 'Test User' do |user|
