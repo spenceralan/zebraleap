7db1c29ddea38fdf48074b23afadce19547d1e55
Anthony Panozzo <panozzaj@gmail.com>
2018-03-28 13:00:33 -0400

    Add basic product buy functionality

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,54 @@
+require 'net/http'
+
+class ProductsController < ApplicationController
+  def buy
+    product = Product.find params[:id]
+    card_token = params[:card_token]
+
+    uri = URI 'http://localhost:12111/v1/customers'
+
+    # Create customer based on card token
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Post.new uri
+      request['Authorization'] = 'Bearer sk_test_123' # set ZebraLeap password
+      request.set_form_data({
+        email: current_user.email,
+        source: card_token,
+      })
+
+      response = http.request request
+
+      if response.code != '200'
+        render json: response.body && return
+      end
+
+      # TODO: parse customer id from the response
+    end
+
+    uri = URI 'http://localhost:12111/v1/charges'
+
+    # Charge customer
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Post.new uri
+      request['Authorization'] = 'Bearer sk_test_123' # set ZebraLeap password
+      request.set_form_data({
+        amount: product.price_in_cents,
+        currency: 'usd',
+        source: 'cus_CCiTI4Tpghl0nK', # customer id from above--hardcoded for now
+        description: "Purchase of #{product.name}",
+      })
+
+      response = http.request request
+
+      if response.code != '200'
+        render json: response.body
+      else
+        if response.body =~ /"paid":true/
+          render json: { success: true }
+        else
+          render json: { error: 'transaction failed' }
+        end
+      end
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.find_or_create_by name: 'Fancy bottle of sparkling water' do |product|
+  product.price_in_cents = 5000
+end
+
+User.find_or_create_by name: 'Test User' do |user|
+  user.email = 'test@example.com'
+end
