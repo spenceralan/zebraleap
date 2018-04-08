ee250969b4d30ff61179aa03b08d5f4989c68c01
shipstar <kyleashipley@gmail.com>
2018-04-08 15:17:54 -0400

    Add ProductsController#buy endpoint for easier stack trace generation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,57 @@ class ProductsController < ApplicationController
   def show
     render json: Product.find(params[:id])
   end
+
+  def buy
+    product = Product.find params[:id]
+    card_token = params[:card_token]
+
+    uri = URI 'https://zebraleap-simple-test.herokuapp.com/v1/customers'
+    customer_id = nil
+
+    # Create customer in ZebraLeap based on card token
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Post.new uri
+      request['Authorization'] = 'Bearer sk_test_123' # set ZebraLeap password
+      request.set_form_data({
+        email: current_user.email,
+        source: card_token,
+      })
+
+      response = http.request request
+
+      if response.code != '200'
+        render json: response.body
+        return
+      end
+
+      customer_id = JSON.parse(response.body)['id']
+    end
+
+    uri = URI 'https://zebraleap-simple-test.herokuapp.com/v1/charges'
+
+    # Charge user
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Post.new uri
+      request['Authorization'] = 'Bearer sk_test_123' # set ZebraLeap password
+      request.set_form_data({
+        amount: product.price_in_cents,
+        currency: 'usd',
+        source: customer_id,
+        description: 'Purchase from Lessonly',
+      })
+
+      response = http.request request
+
+      if response.code != '200'
+        render json: { error: 'transaction failed' }
+      else
+        if response.body =~ /"paid":true/
+          render json: { success: true }
+        else
+          render json: { error: 'transaction failed' }
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'products/:id' => 'products#show'
+  get 'products/:id/buy' => 'products#buy'
 
   root to: redirect('/products/1')
 end
